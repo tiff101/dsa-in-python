93293f69540ec8f9d2706eb1c29d9d01a34e941e
tiff101 <tiffany.jasmine@hotmail.com>
2021-08-23 11:48:04 +1000

    Better separation and IO handling

--- data_io.py
+++ data_io.py
@@ -3,13 +3,20 @@ import os
 
 FILE = os.path.join(os.getcwd(), 'data.yml')
 
+
 def read_yaml(data_file=FILE):
     with open(data_file, 'r') as fp:
         documents = yaml.load(fp)
-
+        # print(documents.keys())
         # TODO: verify document types
-ß
-        # for item, doc in documents.items():
-        #     print(item, ":", doc)
+        if 'food' and 'activities' not in documents.keys():
+            return 'Bad keys. Incorrectly formatted data! Try again?'
 
     return documents
+
+
+def instantiate_yaml_documents(read_output):
+    for item, doc in read_output.items():
+        print(item, ":", doc)
+
+    return 0
