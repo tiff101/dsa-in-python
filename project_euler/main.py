c1630b99520cb52bd20761941e556a99ceb57719
tiff101 <tiffany.jasmine@hotmail.com>
2021-08-23 13:06:34 +1000

    Pythag problem 9 so far lol

--- project_euler/main.py
+++ project_euler/main.py
@@ -1,6 +1,6 @@
 from problems_and_solutions import *
 
-current_function = lattice_paths_15(2)
+current_function = special_pythagorean_triplet()
 
 if __name__ == "__main__":
     result = current_function
--- project_euler/problems_and_solutions.py
+++ project_euler/problems_and_solutions.py
@@ -2,7 +2,7 @@
 This file exists for the purpose of documenting all of Project Euler probs and solutions. 
 Going at a slow pace so we'll see if and how this actually works. 
 """
-
+import math
 
 def lattice_paths_15(grid_size):
     """
@@ -14,3 +14,41 @@ def lattice_paths_15(grid_size):
     num_right_down_moves = grid_size * 2
     routes = num_right_down_moves + 2 ** (grid_size - 1)
     return routes
+
+
+def special_pythagorean_triplet(abc_sum=1000):
+    """
+    https://projecteuler.net/problem=9
+    A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
+    For example, 32 + 42 = 9 + 16 = 25 = 52.
+    There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    Find the product abc.
+    :param abc_sum: natural number
+    :return: a*b*c
+    """
+
+    # 1. find highest possible c squared value - break at 1000 and save
+    for c in range(5, 100):
+        c_squared = c ** 2
+        if c_squared > 1000:
+            break
+    largest_c = c - 1
+    print(largest_c)
+
+    # 2. Now that you have a c limit, find all pythagorean triplets
+    pythagorean_triplets = []
+    for a in range(1, largest_c):
+        # print("a", a)
+        b = a + 1
+        a_squared_plus_b_squared = a**2 + b**2
+        print(f"a={a} b={b} a2+b2={a_squared_plus_b_squared}")
+
+        if a_squared_plus_b_squared == largest_c**2:
+            pythagorean_triplets.append([a, b, largest_c])
+
+
+
+
+            # 3. Identify the triplet that satisfies sum of 1000
+
+    # 4. Return product
